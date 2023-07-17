a4302d48c81594ada28324f65ac614a35ae5ee4a
Son <ungngocsonkt@gmail.com>
2023-07-17 14:58:19 +0700

    feat: respond via message and command

---
+++ app/commands/ping.rb
@@ -0,0 +1,36 @@
+module Ping
+	extend	Discordrb::Commands::CommandContainer
+
+	bot = Discordrb::Commands::CommandBot.new(
+	  token: Rails.application.credentials.dig(:discord, :token),
+	  client_id: Rails.application.credentials.dig(:discord, :client_id),
+	  prefix: '/'
+	)
+	
+	bot.message do |event|
+  if event.message.content.start_with?('/')
+    # Handle command
+    command = event.message.content[1..-1].split(' ').first
+    case command
+    when 'ping'
+      event.respond 'Ping poong!'
+    when 'lenh'
+      event.respond 'Opps!'
+    else
+      event.respond 'Lệnh không hợp lệ!'
+    end
+  else
+    # Handle message
+    case event.message.content
+    when 'Hi'
+      event.respond 'Chào bạn'
+    when 'Hello'
+      event.respond 'Xin chào'
+    else
+      event.respond 'Bot đã nhận được tin nhắn từ bạn!'
+    end
+  end
+end
+
+	bot.run(true)
+end
--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -1,12 +1,14 @@
 require 'discordrb'
 
-
 # rubocop:disable Style/GlobalVars
-ChannelID = 759414764408930326
+CHANNEL_ID = 759414764408930326
 
-Bot = Discordrb::Commands::CommandBot.new(
-    token: Rails.application.credentials.dig(:discord, :token),
-    client_id: Rails.application.credentials.dig(:discord, :client_id),
-    prefix: "/"
+bot = Discordrb::Commands::CommandBot.new(
+  token: Rails.application.credentials.dig(:discord, :token),
+  client_id: Rails.application.credentials.dig(:discord, :client_id),
+  prefix: '/'
 )
+
 Dir["#{Rails.root}/app/commands/*.rb"].each { |file| require file }
+
+bot.run
